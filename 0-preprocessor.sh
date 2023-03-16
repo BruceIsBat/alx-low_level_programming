f8a6f85db80c23de1d9dbb47323f0081e33f136a
BruceIsBat <125538857+BruceIsBat@users.noreply.github.com>
2023-03-16 22:34:22 +0100

    Update 0-preprocessor.sh
    Fy

--- 0-preprocessor.sh
+++ 0-preprocessor.sh
@@ -2,17 +2,12 @@
 
 # This script runs a C file through the preprocessor and saves the result into another file.
 
-# Get the name of the C file
-
+# Get the name of the C file.
 echo "Enter the name of the C file (including the .c extension):"
+read C_FILE
 
-read CFILE
+# Run the C file through the preprocessor and save the result to a new file.
+cpp $C_FILE > c
 
-# Run the C file through the preprocessor and save the result to a new file
-
-cpp $CFILE > c
-
-# Print a message indicating that the preprocessing is complete
-
-echo "Preprocessing of $CFILE is complete. Output saved to c."
-
+# Print a message indicating that the preprocessing is complete.
+echo "Preprocessing of $C_FILE is complete. Output saved to c."
