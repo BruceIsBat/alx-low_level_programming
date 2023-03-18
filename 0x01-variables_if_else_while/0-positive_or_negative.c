2c224fc5753366b43a7bf6554548a3d00c6009a5
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-17 17:06:59 -0700

    Adding README.md to the directory

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d ", n)
+	printf("%d ", n);
 	if (n > 0)
 		printf("is positive\n");
 	else if (n == 0)
