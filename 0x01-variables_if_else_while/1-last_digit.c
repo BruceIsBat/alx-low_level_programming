bf7365c5d632724c5216e6312f4dc359e3dcf78b
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-17 18:12:43 -0700

    Adding README.md to the directory

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d ", n);
+	printf("Last digit of %d is ", n);
 	last_digit = n % 10;
 
 	if (last_digit > 5)
