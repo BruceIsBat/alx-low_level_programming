004c78bdf951e94a0ff0b4af3e5883888e0ba601
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-19 16:23:25 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
 	}
-	for (i = 67; i < 102; i++)
+	for (i = 'a'; i <= 'e' ; i++)
 	{
 		putchar(i);
 	}
