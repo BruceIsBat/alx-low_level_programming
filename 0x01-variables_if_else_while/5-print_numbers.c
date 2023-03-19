58184a45a81d2894346bc0b6a2cf2f36de3f9321
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-19 15:51:27 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,20 +2,16 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single digit numbers of base 10, starting from 0
- * and ending at 9, followed by a new line.
- *
- * Return: Always 0 (Success)
-*/
+ * Return: 0
+ */
 int main(void)
 {
 	char d;
-	
-	for (d = 0; d <= 9; d++)
+
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 	}
-
 	putchar('\n');
 	return (0);
 }
