4ce623e5865c64fd37ab310991c2a34fd1489f45
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-24 11:43:11 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,25 @@ int main(void)
 {
 	int i;
 
-	for (i= 0; i <= 100; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if (i % 3 ==0 && i % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		printf('\n');
 	}
+	printf("\n");
 	return (0);
 }
