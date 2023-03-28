b15bd576c2b44af6d98f2090aae02c17958eaa62
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-27 21:00:21 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int  - check the code
+ * @a:- swaps and stores for b
+ * @b:- swaps and stores for a
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+	swap = *b;
+	*b = *a;
+	*a = swap;
+}
