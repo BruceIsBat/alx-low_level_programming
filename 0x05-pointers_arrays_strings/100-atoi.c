486332c52a2f0dafb426752cc5561432e9ca5ac8
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-28 14:27:11 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,7 +19,7 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
-			int result = (result * 10) + (*s - '0');
+			result = (result * 10) + (*s - '0');
 		}
 		else if (result > 0)
 		{
