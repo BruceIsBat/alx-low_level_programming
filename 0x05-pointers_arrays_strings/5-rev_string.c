c7720029cf404531ae631bf143a4738d5860d9e7
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-27 22:03:54 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @s: defines tge string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int l, z, h;
+	char i;
+
+	for (l = 0, s[l] != '\0'; l++;)
+		z = 0;
+	h = l / 2;
+
+	while (h--)
+	{
+		i = s[l - z - 1];
+		s[l - z - 1] = s[z];
+		s[z] = i;
+		z++;
+	}
+}
