f5d21858c1aa5115d78222ebe5e522e81c1532dc
BruceIsBat <awoteduibrahim@gmail.com>
2023-03-31 05:38:57 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,7 @@
  * _strcmp - Compares two strings.
  * @s1: A pointer to the first string.
  * @s2: A pointer to the second string.
- *
- * Return: An integer less than, equal to, or greater than zero, if s1 is found,
- * respectively, to be less than, to match, or be greater than s2.
+ * Return: An integer less than, equal to, or greater than zero, if s1 is found
  */
 int _strcmp(char *s1, char *s2)
 {
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,4 +14,4 @@ void reverse_array(int *a, int n)
 		a[i] = a[n - 1 - i];
 		a[n - 1 - i] = temp;
 	}
-}	
+}
