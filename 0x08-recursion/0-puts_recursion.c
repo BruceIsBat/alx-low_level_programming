22957acab65abaa9115a00e4d8fecd458853a885
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-05 15:43:16 -0700

    ./0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * _puts_recursion - puts with recursion
+ *
+ * @s: parameter
+ *
+ * Return: the value stored
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
+
+
