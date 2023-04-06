36b4729222aef1c8afaa11810a2da34622a09a59
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-05 17:12:49 -0700

    3-factorial.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - length of string
+ *
+ * @s: parameter
+ *
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+	
