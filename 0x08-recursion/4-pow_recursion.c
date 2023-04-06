b83ab85739e4319f921c0f6e07d4867313465aee
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-05 17:31:43 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _pow_recursion - raise to power
+ *
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: answer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (pow(x, y));
+}
