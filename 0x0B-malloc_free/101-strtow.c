ad7bfe5630f14ef6e2ab3f464ba0c3698f8eae1e
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-11 16:04:57 -0700

    changrd

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,52 +5,48 @@
  * strtow - Split a string into words.
  * @str: The string to split.
  *
- * Return: A pointer to an array of strings (words), or NULL if str is NULL or empty.
- * Each element of the array contains a single word, null-terminated. The last
- * element of the array is NULL.
+ * Return: A pointer to an array of strings
  */
 char **strtow(char *str)
 {
-    int num_words = 0, i = 0, word_index = 0, word_start = 0, word_length = 0;
-    char **words;
+	int num_words = 0, i = 0, word_index = 0, word_start = 0, word_length = 0;
+	char **words;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+		num_words++;
+		while (str[i] != ' ' && str[i] != '\0')
+			i++;
+	}
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ')
-            continue;
-        num_words++;
-        while (str[i] != ' ' && str[i] != '\0')
-            i++;
-    }
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
 
-    words = (char **)malloc((num_words + 1) * sizeof(char *));
-    if (words == NULL)
-        return (NULL);
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ')
-            continue;
-        word_start = i;
-        while (str[i] != ' ' && str[i] != '\0')
-            i++;
-        word_length = i - word_start;
-        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-        if (words[word_index] == NULL)
-        {
-            for (i = 0; i < word_index; i++)
-                free(words[i]);
-            free(words);
-            return (NULL);
-        }
-        strncpy(words[word_index], str + word_start, word_length);
-        words[word_index][word_length] = '\0';
-        word_index++;
-    }
-    words[num_words] = NULL;
-
-    return (words);
+	if (words == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+		word_start = i;
+		while (str[i] != ' ' && str[i] != '\0')
+			i++;
+		word_length = i - word_start;
+		words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+		if (words[word_index] == NULL)
+		{
+			for (i = 0; i < word_index; i++)
+				free(words[i]);
+			free(words);
+			return (NULL);
+		}
+		strncpy(words[word_index], str + word_start, word_length);
+		words[word_index][word_length] = '\0';
+		word_index++;
+	}
+	words[num_words] = NULL;
+	return (words);
 }
