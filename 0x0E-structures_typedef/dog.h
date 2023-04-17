5bd50ab1cfbe8f9df7ea75fcc7549828351c05fb
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-17 12:28:50 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * struct - creating a struct
- * @name: name of the dog
- * @age: its age
- * @owner: the owner
- * Return: success
- */
 struct dog
 {
 	char *name;
