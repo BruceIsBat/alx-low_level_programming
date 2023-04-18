5f0a42661479283339629b431605be8d9e854130
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-17 17:48:03 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,33 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to the dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to the dog to print
+ *
+ * Return: void
+ */
 void print_dog(struct dog *d);
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ *
+ * Return: pointer to the new dog, or NULL if the function fails
+ */
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif /* DOG_H */
