623f43a535eee9618682119b3d308d934827ec5c
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-17 19:03:58 -0700

    0-object_like_macro.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,30 +14,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-		/* allocate memory for the new dog */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* copy the name */
-	new_dog->name = strdup(name);
+	new_dog->name = malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	/* set the age */
+	strcpy(new_dog->name, name);
+
 	new_dog->age = age;
 
-	/* copy the owner */
-	new_dog->owner = strdup(owner);
+
+	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+
+	strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
