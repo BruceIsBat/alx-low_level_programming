adb1a24867c7e773666d3713a212fe4894c9d144
BruceIsBat <awoteduibrahim@gmail.com>
2023-04-27 14:55:30 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: pointer to the pointer of the head of the list.
