dce729fed5eb2587a8a7083a2a465183568aa4cc
BruceIsBat <awoteduibrahim@gmail.com>
2023-05-02 00:57:11 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,58 +1,37 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _sqrt_recursion - natural square root
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
  *
- * @n: parameter
- *
- * Return: anything
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ *         square root.
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
+
+	if (n == 0 || n == 1)
 		return (n);
-	}
-	else
-	{
-		return (_sqrt_helper(n, 1, n));
-	}
+
+	return (sqrt_helper(n, 1));
 }
 
-
 /**
- * _sqrt_helper - Calculates the natural
+ * sqrt_helper - recursively calculates the square root of a number
+ * @n: the number to calculate root of
+ * @i: the current guess for the square root
  *
- * @n: The input number
- * @low: The lower limit
- * @high: The upper limit
- *
- * Return: 0
+ * Return: the natural square root of n
  */
-int _sqrt_helper(int n, int low, int high)
+int sqrt_helper(int n, int i)
 {
-	int mid;
+	if (i * i == n)
+		return (i);
 
-	if (high < low)
-	{
+	if (i * i > n)
 		return (-1);
-	}
-	mid = (low + high) / 2;
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return (_sqrt_helper(n, mid + 1, high));
-	}
-	else
-	{
-		return (_sqrt_helper(n, low, mid - 1));
-	}
+
+	return (sqrt_helper(n, i + 1));
 }
