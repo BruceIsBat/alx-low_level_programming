bca0006babdaf4ca03198ab31c792f6d1f5b5915
Awotedu Ibrahim <awoteduibrahim@gamail.com>
2023-06-23 08:36:37 -0700

    changed

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
-#ifndef 
-#define
+#ifndef CALC_H
+#define CALC_H
+
 /**
  * struct op - Struct op
  *
@@ -8,9 +9,16 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-int calculate(int num1, char op, int num2);
 
-#endif
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* CALC_H */
