66f2568ffe29e5f1b228bac0adf0471b16bbd82f
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-08 10:15:34 -0800

    updated the file

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -12,6 +12,9 @@ int main(void)
     int month;
     int day;
     int year;
+    int mont;
+    int da;
+    int yer;
 
     month = 2;
     day = 29;
@@ -22,6 +25,17 @@ int main(void)
     day = convert_day(month, day);
 
     print_remaining_days(month, day, year);
+
+
+    mont = 3;
+    da = 01;
+    yer = 2008;
+
+    printf("Date: %02d/%02d/%04d\n", mont, da, yer);
+
+    da = convert_day(mont, da);
+
+    print_remaining_days(mont, da, yer);
 
     return (0);
 }
